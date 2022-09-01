c45b239caf393976cea7cdd1372f11dbd45102e2
Савелий <Savka9765@yandex.ru>
2022-09-01 21:01:50 +0300

    Сумма цифр в числе

---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Number(int numberN)
+{
+    if (numberN == 0) return 0;
+    else return numberN % 10 + Number(numberN/10);
+}
+Console.WriteLine($"Сумма цифр в числе = {Number(numberN)}");
